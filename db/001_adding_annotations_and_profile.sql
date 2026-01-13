df292c69533f7e5612e991a59703457f0f4f83cf
jul <julien@tayon.net>
2026-01-13 11:32:52 +0100

    closes #6
    adding annotations
    adding profile

---
+++ db/001_adding_annotations_and_profile.sql
@@ -0,0 +1,8 @@
+ALTER TABLE posts add annotation text[];
+CREATE INDEX idx_handnle on  posts((post->>'{author,handle}'));
+CREATE TABLE profile (
+    profile JSON, 
+    annotation text[],
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
